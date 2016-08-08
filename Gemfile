d3b6a9e589b816546ae67a65236252239a9ad285
gregspagnolo <dwdrummer@gmail.com>
2016-08-08 13:23:10 -0500

    Add image upload

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'devise'
 gem 'omniauth'
 gem 'omniauth-facebook', '3.0.0'
 
+# Use paperclip gem for photo uploads
+gem 'paperclip'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160807223849) do
+ActiveRecord::Schema.define(version: 20160808180955) do
 
   create_table "services", force: :cascade do |t|
     t.string   "title"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20160807223849) do
     t.integer  "delivery_time"
     t.integer  "revisions"
     t.string   "requirements"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
