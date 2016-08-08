228901376d82c5d04dd6841438fe206483f4e670
gregspagnolo <dwdrummer@gmail.com>
2016-08-08 14:02:14 -0500

    Add CRUD for services

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,7 @@
 class Service < ActiveRecord::Base
+
+	belongs_to :user
+	
 	has_attached_file :image, styles: {medium: "300x300>", small: "230x140>", thumb: "100x100>"}
 
 	validates :title, presence: true, length: {maximum:80}
@@ -8,5 +11,5 @@ class Service < ActiveRecord::Base
 	validates :revisions, presence: false
 	validates :requirements, presence: true, length: {maximum:400}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	
+
 end
