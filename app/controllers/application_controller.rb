2ed52aa00d767ad350a95b5cf96d544df6ecbb42
gregspagnolo <dwdrummer@gmail.com>
2016-08-15 15:58:48 -0500

    Fix array method for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   protected
   	def configure_permitted_parameters
-  		devise_parameter_sanitizer.permit(:sign_up) << :full_name
-  		devise_parameter_sanitizer.permit(:account_update) << :full_name << :email << :password << :description 
+  		devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+  		devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :email, :password, :description])
   	end
 end
